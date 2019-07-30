f1a7d60c88479cb5f45b9ec0c550b82fb59de2ab
Shivanand Tripathi <shivanandtripathi02@gmail.com>
2019-07-30 11:40:25 +0530

    Delete AutoBuildTest.bat

--- Project Files/AutoBuildTest.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "C:\Users\320050710\source\repos\Test\Debug/UnitTest1.dll"
- 
